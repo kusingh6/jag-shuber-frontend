151395ce144050481691c4fd0d2fdb6852b8d74e
holly.mcquay <holly.mcquay@CANL-C02VV2XJHTDD.local>
2018-06-07 09:42:53 -0700

    Updating sheriff forms to reflect 'profile'

--- src/app/containers/SheriffCreateProfileForm.tsx
+++ src/app/containers/SheriffCreateProfileForm.tsx
@@ -1,29 +1,33 @@
 import * as React from 'react';
 import { reduxForm, ConfigProps } from 'redux-form';
-import { default as SheriffForm, SheriffFormProps } from '../components/SheriffProfile/SheriffForm';
+import { 
+    default as SheriffProfileForm, 
+    SheriffProfileFormProps 
+} from '../components/SheriffProfile/SheriffProfileForm';
 import { createSheriff } from '../modules/sheriffs/actions';
 import { Sheriff } from '../api/index';
 import { default as FormSubmitButton, SubmitButtonProps } from '../components/FormElements/SubmitButton';
 import { RootState } from '../store';
 import { connect } from 'react-redux';
 
-const formConfig: ConfigProps<any, SheriffFormProps> = {
+const formConfig: ConfigProps<any, SheriffProfileFormProps> = {
     form: 'CreateSheriff',
     onSubmit: (values: Sheriff | any, dispatch, ownProps) => {
-        let newSheriff = SheriffForm.parseSheriffFromValues(values);
+        let newSheriff = SheriffProfileForm.parseSheriffFromValues(values);
         dispatch(createSheriff(newSheriff));
+        // dispatch(createSheriffProfile(newSheriffProfile))
     }
 };
 
-const mapStateToProps = (state: RootState, props: SheriffFormProps) => {
+const mapStateToProps = (state: RootState, props: SheriffProfileFormProps) => {
     return {
        isNewSheriff: true
     };
 };
 
-export default class SheriffCreateForm extends
-    connect<any, {}, SheriffFormProps>
-        (mapStateToProps)(reduxForm(formConfig)(SheriffForm)) {
+export default class SheriffCreateProfileForm extends
+    connect<any, {}, SheriffProfileFormProps>
+        (mapStateToProps)(reduxForm(formConfig)(SheriffProfileForm)) {
     static SubmitButton = (props: Partial<SubmitButtonProps>) => (
         <FormSubmitButton {...props} formName={formConfig.form} />
     )
