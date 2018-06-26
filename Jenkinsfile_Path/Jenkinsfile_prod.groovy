4a94d4cbfa237bb89bbc540669152b7db6c3f55f
kusingh6 <kulpreetsingh.acme@gmail.com>
2018-06-26 14:41:29 -0700

    catching MissingPropertyException workspace

--- Jenkinsfile_Path/Jenkinsfile_prod.groovy
+++ Jenkinsfile_Path/Jenkinsfile_prod.groovy
@@ -32,7 +32,7 @@ def work_space="/var/lib/jenkins/jobs/jag-shuber-tools/jobs/Jag-shuber-prod-depl
       
       try {
       ROUT_CHK = sh (
-      script: """oc project jag-shuber-prod; if [ `oc get route sheriff-scheduling-prod -o=jsonpath='{.spec.to.weight}'` == "100" ]; then `oc get route sheriff-scheduling-prod -o=jsonpath='{.spec.to.name}' > ${workspace}/route-target`; else `oc get route sheriff-scheduling-prod -o=jsonpath='{.spec.alternateBackend[*].name}' > ${workspace}/route-target`; fi""")
+      script: """oc project jag-shuber-prod; if [ `oc get route sheriff-scheduling-prod -o=jsonpath='{.spec.to.weight}'` == "100" ]; then `oc get route sheriff-scheduling-prod -o=jsonpath='{.spec.to.name}' > ${env.WORKSPACE}/route-target`; else `oc get route sheriff-scheduling-prod -o=jsonpath='{.spec.alternateBackend[*].name}' > ${env.WORKSPACE}/route-target`; fi""")
       echo ">> ROUT_CHK: ${ROUT_CHK}"
       // Deploy Fontend Image to the production environment
       openshiftDeploy deploymentConfig: APP_NAME_F+"-"+newTarget, namespace: "${PROJECT_PREFIX}"+"-"+environment, waitTime: '900000'
@@ -107,7 +107,7 @@ def work_space="/var/lib/jenkins/jobs/jag-shuber-tools/jobs/Jag-shuber-prod-depl
 
   //def input = readFile("${work_space}/route-target") 
     currentTarget = sh (
-      script: """cat ${workspace}/route-target | awk -F"-" '{print \$2}' """)
+      script: """cat ${env.WORKSPACE}/route-target | awk -F"-" '{print \$2}' """)
       // echo ">> ROUT_CHK: ${ROUT_CHK}"
     return currentTarget
   }
